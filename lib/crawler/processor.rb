49464bf86c3dd5d610d15e5bfd7e30cce40fc28f
Jason Luong <jasonluong@Jasons-MacBook-Pro.local>
2016-08-31 12:30:33 +0100

    added raw processor

---
+++ lib/crawler/processor.rb
@@ -0,0 +1,43 @@
+class Processor
+  attr_reader :processed_links
+
+  def initialize
+    @processed_links = []
+    @domain = ""
+  end
+
+  def process_links(links = {})
+    @domain = links["url"]
+    unprocessed_links = links["links_arr"]
+
+    unprocessed_links.each do |link|
+      if link.chars.first == "/"
+        if link.length > 1
+          if !@processed_links.include? link
+            @processed_links << link
+          end
+        end
+      end
+    end
+  end
+
+  def create_links
+    links = []
+    @processed_links.each do |link|
+      links << @domain + link
+    end
+
+    links
+  end
+end
+
+
+dummy_info = { "url" => "http://www.makersacademy.com",
+                "title" => "MakersAcademy",
+                "links_arr" => ["https://www.mozilla.org/en-GB/firefox/new/", "http://www.google.com/chrome/", "/", "/graduates/", "/employers/", "/curriculum/", "/about-us/", "/", "/graduates/", "/employers/", "/curriculum/", "/about-us/", "/apply", "/graduates/", "/employers/", "/curriculum/", "/curriculum/", "http://blog.makersacademy.com/changingourprices/", "http://blog.makersacademy.com/changingourprices/", "/payment/", "/apply", "/about-us/", "http://www.github.com/makersacademy", "http://www.facebook.com/MakersAcademy", "http://twitter.com/makersacademy", "https://www.youtube.com/user/makersacademy", "http://blog.makersacademy.com/", "https://www.switchup.org/bootcamps/makers-academy", "https://www.quora.com/makers-academy", "https://www.coursereport.com/schools/makers-academy#/reviews", "mailto:contact@makersacademy.com", "tel:02038178870", "http://makers-academy.breezy.hr/p/20f2efaba8d1-coach--engineer", "/code-of-conduct/", "/terms/", "/remote-terms/", "/privacy/", "https://mixpanel.com/f/partner"] }
+
+pro = Processor.new
+
+pro.process_links(dummy_info)
+pro.processed_links
+puts pro.create_links
